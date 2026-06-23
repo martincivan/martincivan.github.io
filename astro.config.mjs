5cb252ba2f9dacaede643b00e40aeb8d73869197
Martin Civáň <martin.civan5@gmail.com>
2026-06-24 00:59:05 +0200

    what do I do columns

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://martincivan.github.io',
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'sk'],
